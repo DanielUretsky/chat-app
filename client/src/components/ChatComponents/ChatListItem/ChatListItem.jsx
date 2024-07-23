e35a806862c309ac55ec494ea88ff7e023a11463
Daniel <daniel353a3@gmail.com>
2024-07-23 22:49:17 +0300

    added property title, changed css

--- client/src/components/ChatComponents/ChatListItem/ChatListItem.jsx
+++ client/src/components/ChatComponents/ChatListItem/ChatListItem.jsx
@@ -8,13 +8,14 @@ import { UserIcon } from '../../Icons/UserIcon/UserIcon';
 import deleteChatIcon from '../../../assets/icons/delete.png';
 
 import './ChatListItem.css';
+import { useTheme } from '../../../context/ThemeContext';
 
 
 export const ChatListItem = ({ chat }) => {
   const member = chat?.members[0];
-
-  const dispatch = useDispatch();
+  const { theme } = useTheme();
   const [isHover, setIsHover] = useState(false);
+  const dispatch = useDispatch();
 
   const openDeletedChatModalHandler = async (e) => {
     e.stopPropagation();
@@ -29,29 +30,30 @@ export const ChatListItem = ({ chat }) => {
 
   return (
     <div
-      className="chat-list-item-container"
+      className={`chat-list-item-container ${theme === 'light' && 'chat-list-item-container__light'}`}
       onClick={() => dispatch(actions.setCurrentChat(chat))}
       onMouseEnter={() => setIsHover(prev => !prev)}
       onMouseLeave={() => setIsHover(prev => !prev)}
+      title={`${member?.username} \n${member?.email}`}
     >
+
       <UserIcon
         userImage={member?.image}
         className={'chat-list-item-container__user-image'}
       />
-      <div className="chat-list-item-user">
-        {isHover &&
-          <img
-            className="chat-list-item-container__delete-icon"
-            src={deleteChatIcon}
-            alt="delete"
-            onClick={openDeletedChatModalHandler}
-            title='Delete'
-          />
-        }
-        <span>{member?.username}</span>
-        <p className="chat-list-item-last-message">
 
-        </p>
+      {isHover &&
+        <img
+          className="chat-list-item-container__delete-icon"
+          src={deleteChatIcon}
+          alt="delete"
+          onClick={openDeletedChatModalHandler}
+          title='Delete'
+        />
+      }
+      
+      <div className="chat-list-item-user">
+        <span className=''>{member?.username}</span>
       </div>
     </div>
   )
