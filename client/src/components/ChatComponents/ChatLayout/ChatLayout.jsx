21441c3b8f04de5f576d0d61114c90a02b72fdae
Daniel <daniel353a3@gmail.com>
2024-07-23 22:48:30 +0300

    some changes, changed css

--- client/src/components/ChatComponents/ChatLayout/ChatLayout.jsx
+++ client/src/components/ChatComponents/ChatLayout/ChatLayout.jsx
@@ -1,5 +1,6 @@
-import { useContext, useEffect, useRef, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useContext, useEffect, useRef, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useTheme } from '../../../context/ThemeContext';
 
 import { getChatMessages } from '../../../redux/slices/chat/chatSlice';
 import { SocketContext } from '../../../context/SocketContext';
@@ -11,6 +12,7 @@ import { ChatLayoutTools } from './ChatLayoutTools/ChatLayoutTools';
 import { ModalRoot } from '../../Modals/ModalRoot';
 import { AnimatePresence } from 'framer-motion';
 import { DeleteMessageModal } from '../../Modals/DeleteMessageModal/DeleteMessageModal';
+
 import './ChatLayout.css';
 
 export const ChatLayout = ({ currentChat }) => {
@@ -22,27 +24,72 @@ export const ChatLayout = ({ currentChat }) => {
   const isDeleteModalOpen = useSelector(state => state.message.isDeleteModalOpen);
 
   const { socket } = useContext(SocketContext);
+  const { theme } = useTheme();
   const [messages, setMessages] = useState([]);
   const [messageData, setMessageData] = useState("");
+
   const messageEndListRef = useRef(null);
- 
+  const messagesRef = useRef([]);
   // ---- PLAN ----
   // realized mechanism to save messages not on button click !important
-  useEffect(() => {
-  
-    dispatch(getChatMessages(room));
-  }, [currentChat, room, messages, dispatch]);
-  
+  // useEffect(() => {
 
-  
+  //   dispatch(getChatMessages(room));
+
+  // }, [currentChat, room, messages, dispatch]);
+
+  // useEffect(() => {
+  //   const getMessages = async () => {
+  //     await dispatch(getChatMessages(room)).unwrap();
+  //     setMessages(currentMessages);
+
+  //   }
+  //   getMessages();
+  //   console.log(messages);
+  // }, [])
+
   useEffect(() => {
-    dispatch(getChatMessages(room))
+    const getMessages = async () => {
+      const messagesArray = await dispatch(getChatMessages(room)).unwrap();
+      console.log(messagesArray);
+      setMessages(messagesArray);
+
+      //console.log(messages);
+    }
+    getMessages()
+  }, [currentChat, room, dispatch])
+
+  useEffect(() => {
+
+    dispatch(getChatMessages(room));
+
     if (socket) {
       socket?.emit('join_room', room);
-      socket?.on('receive_message', (message) => {
-        if (message.roomID === room) setMessages(prevMessages => [...prevMessages, message]);
+
+      socket?.on('user-joined', (room) => {
+        //console.log(room);
+        // console.log(currentMessages);
       });
 
+      socket?.on('receive_message', (message) => {
+        if (message.roomID === room) {
+          // const updatedMessages = [...messagesRef.current, message];
+          // messagesRef.current = updatedMessages;
+          //console.log(',esese', message);
+          setMessages(prev => [...prev, {
+            body: {
+              deleteForEveryone: false,
+              deletedForSender: false,
+              images: message.images,
+              text: message.text,
+              timeSend: message.timeSend
+            },
+            sender: message._id,
+          }])
+        }
+      });
+
+      console.log(messages);
       socket?.on('message-deleted', (room) => {
         dispatch(getChatMessages(room));
       });
@@ -58,18 +105,22 @@ export const ChatLayout = ({ currentChat }) => {
       socket?.off('message-updated');
       socket?.emit('leave_room', room);
 
-      setMessages([]);
+      socket.on('room_leaved', (room) => {
+        // console.log(messagesRef.current);
+      });
+
+     
     }
-  }, [socket, currentChat, room, dispatch])
+  }, [socket, currentChat, room, messages, dispatch])
 
   // messages to see message in real time, should be changed
 
   useEffect(() => {
-    messageEndListRef.current?.scrollIntoView({behavior: 'auto'});
+    messageEndListRef.current?.scrollIntoView({ behavior: 'auto' });
   }, [currentMessages])
   return (
     <div
-      className="chat-layout-container"
+      className={`chat-layout-container ${theme === 'light' && 'chat-layout-container__light'}`}
     >
       <AnimatePresence>
         {
@@ -84,12 +135,12 @@ export const ChatLayout = ({ currentChat }) => {
 
       <div className="chat-layout-main">
         {
-          currentMessages?.map((message) => {
-            if (!message.body.deletedForEveryone &&
-              !(currentUser?._id === message.sender && message.body.deletedForSender)) {
+          messages?.map((message) => {
+            if (!message?.body?.deletedForEveryone &&
+              !(currentUser?._id === message?.sender && message.body?.deletedForSender)) {
               return (
                 <MessageItem
-                  key={message._id}
+                  key={message?._id}
                   message={message}
                   currentUserId={currentUser?._id}
                   setMessageData={setMessageData}
@@ -104,6 +155,7 @@ export const ChatLayout = ({ currentChat }) => {
       <ChatLayoutTools
         messageData={messageData}
         setMessageData={setMessageData}
+        setMessages={setMessages}
       />
     </div>
 
