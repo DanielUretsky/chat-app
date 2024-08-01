89963aeddf45b38606223703df43ccd050a94128
Daniel <daniel353a3@gmail.com>
2024-08-02 01:46:12 +0300

    tranferred listeners for sockets join_room, leave_room, added new listener send-restore-request

--- client/src/pages/Chat/Chat.jsx
+++ client/src/pages/Chat/Chat.jsx
@@ -1,5 +1,7 @@
-import { useTheme } from "../../context/ThemeContext";
-import { useSelector } from "react-redux";
+import { useContext, useEffect } from "react";
+import { SocketContext } from "../../context/SocketContext";
+
+import { useDispatch, useSelector } from "react-redux";
 import { AnimatePresence } from "framer-motion";
 
 import { SideBar } from "../../components/ChatComponents/SideBar/SideBar";
@@ -7,17 +9,47 @@ import { ChatLayout } from "../../components/ChatComponents/ChatLayout/ChatLayou
 import { NoChatsLayout } from "../../components/ChatComponents/NoChatsLayout/NoChatsLayout";
 
 import './Chat.css';
+import { restoreChat, setUserChats } from "../../redux/slices/chat/chatSlice";
 
 export const Chat = () => {
+  const { socket } = useContext(SocketContext);
+
   const currentChat = useSelector(state => state.chat.currentChat);
-  const { theme } = useTheme();
+  const currentUser = useSelector(state => state.user.user);
+  const dispatch = useDispatch();
+  const room = currentChat?._id;
+
+  useEffect(() => {
+    room && socket?.emit('join_room', room);
+
+    socket?.on(`send-restore-request-${currentUser?._id}`, async (fromUsername, deletedChatId) => {
+      if (confirm(`${fromUsername} wants to restore chat with him`)) {
+        console.log(deletedChatId);
+        await dispatch(restoreChat(deletedChatId)).unwrap();
+
+        socket?.emit('restore-chat');
+
+       await dispatch(setUserChats()).unwrap();
+      }
+    });
+    return () => {
+      room && socket?.emit('leave_room', room);
+      socket?.off(`send-restore-request-${currentUser?._id}`);
+    }
+
+  }, [socket, room]);
+
   return (
     <div className="chat-wrapper">
       <div className="chat-container">
         <SideBar />
         <AnimatePresence>
           {
-            currentChat ? <ChatLayout currentChat={currentChat} /> : <NoChatsLayout />
+            currentChat ?
+              <ChatLayout
+                currentChat={currentChat}
+              /> :
+              <NoChatsLayout />
           }
         </AnimatePresence>
       </div>
