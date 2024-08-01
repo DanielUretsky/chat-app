541077e41666505b6c3419308d281b19385b5fb5
Daniel <daniel353a3@gmail.com>
2024-08-02 01:25:33 +0300

    added wrapper component with new interceptor to capture 401 status code for user logout

--- client/src/axios/axiosConfig.js
+++ client/src/axios/axiosConfig.js
@@ -1,19 +1,44 @@
 import axios from "axios";
-import { getCookie, setCookie } from "../services/cookiesService";
+import { getCookie, removeCookie, setCookie } from "../services/cookiesService";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { actions } from "../redux/slices/userSlice";
 
 
 export const baseRequest = axios.create({
     baseURL: "http://localhost:4080",
-   
+
     headers: {
         Accept: `application/json`
     }
 });
 
-
-// ---- INTERCEPTORS ----
 baseRequest.interceptors.request.use((config) => {
     config.headers.Authorization = `Bearer ${getCookie("accessToken")}`;
     return config;
 });
+
+export const AxiosInterceptorGate = ({ children }) => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const unauthorizedInterceptor = baseRequest.interceptors.response.use((response) => {
+            return response;
+        }, (err) => {
+            if (err.response.status === 401) {
+                removeCookie("accessToken");
+                dispatch(actions.logout(null));
+                navigate('/login');
+
+            }
+            return Promise.reject(err);
+        })
+
+        return () => baseRequest.interceptors.response.eject(unauthorizedInterceptor);
+  
+    }, [])
+    return children
+}
 
