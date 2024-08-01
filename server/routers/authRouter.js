32c5cc1f5df68ac319084f9a67ac8a936a9301a7
Daniel <daniel353a3@gmail.com>
2024-08-02 01:13:19 +0300

    removed logout route

--- server/routers/authRouter.js
+++ server/routers/authRouter.js
@@ -4,7 +4,6 @@ const router = express.Router();
 
 router.post('/registration', authController.registration);
 router.post('/login', authController.login);
-//router.get('/logout', authController.logout);
 router.get('/authenticate', authController.authenticate);
 
 module.exports = router; 
