ab16aaedfd71e87978b46aa9cf0a6225b919936f
Daniel <daniel353a3@gmail.com>
2024-08-12 02:29:30 +0300

    tranfer data for notification, some changes

--- client/src/components/DeletedChatListItem/DeletedChatListItem.jsx
+++ client/src/components/DeletedChatListItem/DeletedChatListItem.jsx
@@ -1,29 +1,45 @@
 import { useContext, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+
 import { useTheme } from '../../context/ThemeContext';
+import { SocketContext } from '../../context/SocketContext';
 
 import { UserIcon } from '../Icons/UserIcon/UserIcon';
+
+import { restoreChat, setDeletedUserChats, setUserChats } from '../../redux/slices/chat/chatSlice';
+import { sendRestoreChatRequestNotification } from '../../services/notifiationService';
+import { getCurrentTime } from '../../utils/getCurrentTime';
 
 import restoreChatIcon from '../../assets/icons/restore.png';
 
 import './DeletedChatListItem.css';
-import { restoreChat, setDeletedUserChats, setUserChats } from '../../redux/slices/chat/chatSlice';
-import { SocketContext } from '../../context/SocketContext';
+
 export const DeletedChatListItem = ({ deletedChat }) => {
   const deletedChatMember = deletedChat.members && deletedChat.members[0];
-  const currentUser = useSelector(state => state.user.user);
+
   const { theme } = useTheme();
   const{ socket } = useContext(SocketContext);
-
-  const [isHover, setIsHover] = useState(false);
   
+  const [isHover, setIsHover] = useState(false);
+  const currentUser = useSelector(state => state.user.user);
+
   const dispatch = useDispatch()
 
-  const restoreChatHndler = async(e) => {
-    console.log(deletedChat._id);
+  const sendRestoreChatNotificationHandler = async(e) => {
     e.stopPropagation();
+
     if(deletedChat.deletedFor === 'all') {
-      socket?.emit('restore-chat-request', deletedChatMember._id, currentUser?.username, deletedChat?._id);
+      const notificationTransferData = {
+        notificationType: 'restore-chat',
+        dateSend: new Date().toLocaleDateString(),
+        timeSend: getCurrentTime(),
+        chatId: deletedChat?._id,
+        sender:  currentUser?._id,
+        user: deletedChatMember?._id,
+      };
+      
+      await sendRestoreChatRequestNotification(notificationTransferData);
+      socket?.emit('restore-chat-request', deletedChatMember._id);
     } 
     else {
       await dispatch(restoreChat(deletedChat?._id)).unwrap();
@@ -48,7 +64,7 @@ export const DeletedChatListItem = ({ deletedChat }) => {
           className="chat-list-item-container__restore-icon"
           src={restoreChatIcon}
           alt="restore"
-          onClick={restoreChatHndler}
+          onClick={sendRestoreChatNotificationHandler}
           title='Restore chat'
         />
       }
